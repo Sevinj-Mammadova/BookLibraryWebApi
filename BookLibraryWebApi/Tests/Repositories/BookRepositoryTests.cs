4a943a167df58a2553d98d014eddff36d7cbe3d8
Sevinj-Mammadova <sevinj.a.mammadova@socardownstream.az>
2025-07-22 15:50:27 +0400

    Added tests

---
+++ BookLibraryWebApi/Tests/Repositories/BookRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using BookLibraryWebApi.Domain.Entities;
+using BookLibraryWebApi.Infrastructure.Data;
+using BookLibraryWebApi.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace BookLibraryWebApi.Tests.Repositories
+{
+    public class BookRepositoryTests
+    {
+        private async Task<DataContext> GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString())
+                .Options;
+            var context = new DataContext(options);
+
+            context.Books.AddRange(new List<Book>
+            {
+                new Book { Id = 1, Author = "A", IsAvailable = true },
+                new Book { Id = 2, Author = "B", IsAvailable = true }
+            });
+            await context.SaveChangesAsync();
+            return context;
+        }
+        [Fact]
+        public async Task AddBookAsync_ShouldAddBook()
+        {
+            //arrange
+            var context = await GetInMemoryDbContext();
+            var bookRepo = new BookRepository(context);
+
+            //act
+            var createdBook = await bookRepo.AddBookAsync(new Book { Id = 3, Author = "C", IsAvailable = true });
+            //assert
+            Assert.NotNull(createdBook);
+            Assert.Equal(3, await context.Books.CountAsync());
+            Assert.Equal("C", createdBook.Author);
+        }
+    }
+}
--- BookLibraryWebApi/Tests/Services/LibraryServiceTests.cs
+++ BookLibraryWebApi/Tests/Services/LibraryServiceTests.cs
@@ -5,6 +5,7 @@ using BookLibraryWebApi.Application.Interfaces;
 using BookLibraryWebApi.Domain.Entities;
 using System.Threading.Tasks;
 using BookLibraryWebApi.Infrastructure.Repositories;
+using AutoMapper;
 
 namespace BookLibraryWebApi.Tests.Services
 {
@@ -12,45 +13,72 @@ namespace BookLibraryWebApi.Tests.Services
     {
         private readonly Mock<IBookRepository> _bookRepoMock = new();
         private readonly Mock<IBorrowRecordRepository> _borrowRepoMock = new();
-        private readonly Mock<AutoMapper.IMapper> _mapperMock = new();
-
+        private readonly Mock<IMapper> _mapperMock = new();
         private readonly LibraryService _libraryService;
 
         public LibraryServiceTests()
         {
-            _libraryService = new LibraryService(
-                _bookRepoMock.Object,
-                _mapperMock.Object,
-                _borrowRepoMock.Object
-            );
+            _libraryService = new LibraryService(_bookRepoMock.Object, _mapperMock.Object, _borrowRepoMock.Object);
         }
 
         [Fact]
-        public async Task BorrowBookAsync_ShouldReturnNull_IfBookNotAvailable()
+        public async Task BorrowBookAsync_ShouldReturnNull_IfNotAvailable()
         {
+            //arrange
             _bookRepoMock.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
-                .ReturnsAsync(new Book { IsAvailable = false });
-
+                .ReturnsAsync(new Book { Id = 1, IsAvailable = false });
+            //act
             var result = await _libraryService.BorrowBookAsync(1, 1);
-
+            //assert
             Assert.Null(result);
         }
 
         [Fact]
-        public async Task BorrowBookAsync_ShouldCreateBorrowRecord_IfBookAvailable()
+        public async Task BorrowBookAsync_ShouldReturnNotNull_IfAvailable()
         {
-            var book = new Book { Id = 1, IsAvailable = true };
-
-            _bookRepoMock.Setup(r => r.GetBookByIdAsync(It.IsAny<int>())).ReturnsAsync(book);
-            _bookRepoMock.Setup(r => r.UpdateBookAsync(It.IsAny<Book>())).Returns(Task.CompletedTask);
-            _borrowRepoMock.Setup(r => r.AddBorrowRecordAsync(It.IsAny<BorrowRecord>())).Returns(Task.CompletedTask);
-
+            //arrange
+            _bookRepoMock.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(new Book { Id = 1, IsAvailable = true });
+            _bookRepoMock.Setup(r => r.UpdateBookAsync(It.IsAny<Book>()))
+                .Returns(Task.CompletedTask);
+            _borrowRepoMock.Setup(r => r.AddBorrowRecordAsync(It.IsAny<BorrowRecord>()))
+                .Returns(Task.CompletedTask);
+            //act
             var result = await _libraryService.BorrowBookAsync(1, 1);
-
+            //assert
             Assert.NotNull(result);
             Assert.Equal(1, result.BookId);
-            _bookRepoMock.Verify(r => r.UpdateBookAsync(It.Is<Book>(b => !b.IsAvailable)), Times.Once);
             _borrowRepoMock.Verify(r => r.AddBorrowRecordAsync(It.IsAny<BorrowRecord>()), Times.Once);
+            _bookRepoMock.Verify(r => r.UpdateBookAsync(It.IsAny<Book>()), Times.Once);
+        }
+        [Fact]
+        public async Task ReturnBookAsync_ShouldReturnFalse_IfAvailable()
+        {
+            //arrange
+            _bookRepoMock.Setup(f => f.GetBookByIdAsync(It.IsAny<int>())).ReturnsAsync(new Book { IsAvailable = true });
+            //act
+            var result = await _libraryService.ReturnBookAsync(1, 1);
+            //assert 
+            Assert.False(result);
+        }
+        [Fact]
+        public async Task ReturnBookAsync_ShouldReturnTrue_IfNotAvilable()
+        {
+            //arrange
+            _bookRepoMock.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(new Book { IsAvailable = false });
+            _bookRepoMock.Setup(r => r.UpdateBookAsync(It.IsAny<Book>()))
+                .Returns(Task.CompletedTask);
+            _borrowRepoMock.Setup(r => r.GetActiveBorrowRecordAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new BorrowRecord { ReturnDate = null });
+            _borrowRepoMock.Setup(r => r.UpdateBorrowRecordAsync(It.IsAny<BorrowRecord>()))
+                .Returns(() => Task.CompletedTask);
+            //act
+            var result = await _libraryService.ReturnBookAsync(1, 1);
+            //assert
+            Assert.True(result);
+            _bookRepoMock.Verify(r => r.UpdateBookAsync(It.IsAny<Book>()), Times.Once());
+            _borrowRepoMock.Verify(f => f.UpdateBorrowRecordAsync(It.IsAny<BorrowRecord>()), Times.Once);
         }
     }
 }
